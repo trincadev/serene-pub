49ca06bb78577b525c0dcdc90f1223cd53a0b358
Jody Doolittle <jody@chariotdev.net>
2025-06-11 23:03:13 -0700

    Update readme

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,6 @@
 import { drizzle } from "drizzle-orm/better-sqlite3"
 import Database from "better-sqlite3"
 import * as schema from "./schema"
-import envPaths from "env-paths"
 import { migrate } from "drizzle-orm/better-sqlite3/migrator"
 import * as dbConfig from "./drizzle.config"
 import type { MigrationConfig } from "drizzle-orm/migrator"
