861d7be8999e1340d5ffadcd26a488f400a9bdb5
Jody Doolittle <jody@chariotdev.net>
2025-06-17 06:49:36 -0700

    Remove extra break-point

--- drizzle/0009_natural_marten_broadcloak.sql
+++ drizzle/0009_natural_marten_broadcloak.sql
@@ -1,4 +1,5 @@
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
+PRAGMA foreign_keys=OFF;
+--> statement-breakpoint
 CREATE TABLE `__new_chat_characters` (
 	`chat_id` integer NOT NULL,
 	`character_id` integer,
@@ -8,9 +9,11 @@ CREATE TABLE `__new_chat_characters` (
 	FOREIGN KEY (`character_id`) REFERENCES `characters`(`id`) ON UPDATE no action ON DELETE set null
 );
 --> statement-breakpoint
-INSERT INTO `__new_chat_characters`("chat_id", "character_id", "position", "is_active") SELECT "chat_id", "character_id", "position", "is_active" FROM `chat_characters`;--> statement-breakpoint
-DROP TABLE `chat_characters`;--> statement-breakpoint
-ALTER TABLE `__new_chat_characters` RENAME TO `chat_characters`;--> statement-breakpoint
+INSERT INTO `__new_chat_characters`("chat_id", "character_id", "position", "is_active") SELECT "chat_id", "character_id", "position", "is_active" FROM `chat_characters`;
+--> statement-breakpoint
+DROP TABLE `chat_characters`;
+--> statement-breakpoint
+ALTER TABLE `__new_chat_characters` RENAME TO `chat_characters`;
 --> statement-breakpoint
 CREATE TABLE `__new_chats` (
 	`id` integer PRIMARY KEY NOT NULL,
@@ -25,8 +28,10 @@ CREATE TABLE `__new_chats` (
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-INSERT INTO `__new_chats`("id", "name", "is_group", "user_id", "created_at", "updated_at", "scenario", "metadata", "group_reply_strategy") SELECT "id", "name", "is_group", "user_id", "created_at", "updated_at", "scenario", "metadata", "group_reply_strategy" FROM `chats`;--> statement-breakpoint
-DROP TABLE `chats`;--> statement-breakpoint
+INSERT INTO `__new_chats`("id", "name", "is_group", "user_id", "created_at", "updated_at", "scenario", "metadata", "group_reply_strategy") SELECT "id", "name", "is_group", "user_id", "created_at", "updated_at", "scenario", "metadata", "group_reply_strategy" FROM `chats`;
+--> statement-breakpoint
+DROP TABLE `chats`;
+--> statement-breakpoint
 ALTER TABLE `__new_chats` RENAME TO `chats`;
 --> statement-breakpoint
 PRAGMA foreign_keys=ON;
