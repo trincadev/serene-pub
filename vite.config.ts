d2e355c9222091cb8929e2705e9c9a749830c742
Jody Doolittle <jody@chariotdev.net>
2025-06-18 04:54:42 -0700

    Restore actual versions

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import pkg from "./package.json"
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	define: {
-		__APP_VERSION__: JSON.stringify("0.1.0"), //JSON.stringify(pkg.version)
-		__APP_VERSION_DISPLAY__: JSON.stringify("v0.1.0-alpha") //JSON.stringify(`v${pkg.version}-alpha`)
+		__APP_VERSION__: JSON.stringify(pkg.version),
+		__APP_VERSION_DISPLAY__: JSON.stringify(`v${pkg.version}-alpha`)
 	}
 })
